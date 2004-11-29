77f312682fa63b47a52e5fab33afae99446d29c2
romu <romu@a439c253-ffe1-0310-bfb1-e14c3bc41814>
2004-11-29 18:10:50 +0000

    made a copy

---
+++ tags/Start_Stop/_test/Program.cs
@@ -0,0 +1,27 @@
+﻿#region Using directives
+
+using System;
+using System.Text;
+using Physalis.Framework;
+using Physalis.Utils;
+
+#endregion
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+//            TracesProvider.Initialize(new TraceOutput("169.254.25.129", 6666));
+            TracesProvider.Initialize(new TraceOutput("192.168.0.2", 6666));
+
+            string[] initials = new string[] {@".\Bundle.dll"};
+            Starter.Start(initials);
+
+            Starter.Shutdown(0);
+
+            TracesProvider.TracesOutput.OutputTrace("End");
+        }
+    }
+}
